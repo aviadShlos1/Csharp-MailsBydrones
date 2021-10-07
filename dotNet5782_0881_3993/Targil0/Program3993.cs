476a511306fa9f311b50e6d82b1128506bff860d
evilevi <evilevi20@gmail.com>
2021-10-07 22:59:17 +0300

    i added welcome

---
+++ dotNet5782_0881_3993/Targil0/Program3993.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome3993();
+            Welcome0881();
+            Console.ReadKey();
+        }
+        static partial void Welcome0881();
+        private static void Welcome3993()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("{0}, welcome to my first console application", name);
+        }
+    }
+}
