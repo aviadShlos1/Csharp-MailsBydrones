6e0f367dae7003b9d2f31a5e96b25d76693cdffe
Aviad Shlosberg <ashlosbe@g.jct.ac.il>
2021-10-07 23:27:48 +0300

    i added my welcome too

---
+++ dotNet5782_0881_3993/Targil0/Program0881.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Targil0
+{
+    partial class Program
+    {
+
+        static partial void Welcome0881()
+        {
+            Console.WriteLine("I am also here!");
+        }
+
+    }
+}
+
