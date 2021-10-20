afce06caf3fd9a68bc24cf92d25a50b63e7f1bd2
evilevi <evilevi20@gmail.com>
2021-10-20 22:46:54 +0300

    structers.1

---
+++ dotNet5782_0881_3993/DAL/Drone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        public struct Drone
+        {
+            int Id;
+            string Model;
+            WeightCategories MaxWeight;
+            DroneStatuses Status;
+            double Battery;
+        }
+    }
+}
