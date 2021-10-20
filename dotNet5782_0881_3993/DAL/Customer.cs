1c3150e7679ac98a5ae169da21a8fc9cc32f7ebf
evilevi <evilevi20@gmail.com>
2021-10-20 22:23:24 +0300

    Targil1

---
+++ dotNet5782_0881_3993/DAL/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace IDAL
+{
+    namespace DO
+    {
+        public struct Customer
+        {
+            int Id;
+            string Name;
+            string Phone;
+            double Longitude;
+            double Lattitude;
+        }
+    }
+}
