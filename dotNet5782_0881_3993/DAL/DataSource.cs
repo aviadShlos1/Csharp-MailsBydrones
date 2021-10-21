e7f9843562a8a2d3bb5065c153b956087fe2bbd2
aviad shlosberg <ashlosbe@g.jct.ac.il>
2021-10-21 10:31:49 +0300

    DataSource

---
+++ dotNet5782_0881_3993/DAL/DataSource.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalObject
+{
+    class DataSource
+    {
+        internal static IDAL.DO.Drone[] drones = new IDAL.DO.Drone[10];
+        internal static IDAL.DO.Station[] stations = new IDAL.DO.Station[5];
+    }
+}
