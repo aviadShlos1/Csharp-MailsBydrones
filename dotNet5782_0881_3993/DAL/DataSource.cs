d4a87402f6b9c68e2273d66b106dece294524be8
evilevi <evilevi20@gmail.com>
2021-10-21 10:53:05 +0300

    Arrays

--- dotNet5782_0881_3993/DAL/DataSource.cs
+++ dotNet5782_0881_3993/DAL/DataSource.cs
@@ -6,9 +6,19 @@ using System.Threading.Tasks;
 
 namespace DalObject
 {
-    class DataSource
+    struct DataSource
     {
         internal static IDAL.DO.Drone[] drones = new IDAL.DO.Drone[10];
         internal static IDAL.DO.Station[] stations = new IDAL.DO.Station[5];
+        internal static IDAL.DO.Customer[] customers = new IDAL.DO.Customer[100];
+        internal static IDAL.DO.Parcel[] parcels = new IDAL.DO.Parcel[1000];
+        internal struct Config 
+        {
+            internal static int indexDrone = 0;
+            internal static int indexStation = 0;
+            internal static int indexCustomer = 0;
+            internal static int indexParcel = 0;
+            public int RunId;
+        }
     }
 }
