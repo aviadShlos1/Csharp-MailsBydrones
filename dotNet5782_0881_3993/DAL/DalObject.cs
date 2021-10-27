31a4412ceebd7e7c8bf6c3fc39ed05029fa2d767
evilevi <evilevi20@gmail.com>
2021-10-27 12:33:16 +0300

    Add

--- dotNet5782_0881_3993/DAL/DalObject.cs
+++ dotNet5782_0881_3993/DAL/DalObject.cs
@@ -9,24 +9,32 @@ namespace DalObject
 {
     class DalObject
     {
+        DalObject() { DataSource.Initialize(); }
         public void AddDrone(Drone newDrone)
         {
             DataSource.Drones.Add(newDrone);
         }
         public void AddCustomer(Customer newCustomer)
         {
-
+            DataSource.Customers.Add(newCustomer);
         }
         public void AddParcel(Parcel newParcel)
         {
-            
+            DataSource.Parcels.Add(newParcel);
         }
         public void AddStation(Station newStation)
         {
+            DataSource.Stations.Add(newStation);
+        }
+        public void ConnectDroneToParcel(Parcel p,Drone d)
+        {
+            p.DroneId = d.Id;
+            p.Scheduled
+        }
+        public void PickUpParcel(Parcel p, Drone d)
+        {
 
         }
-        public void Update()
-        { }
         public void Display()
         { }
         public void ListDisplay()
