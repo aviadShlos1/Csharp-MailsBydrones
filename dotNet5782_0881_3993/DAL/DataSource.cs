80547db0f12c3f3ac7d39567797919a1166f5410
aviad shlosberg <ashlosbe@g.jct.ac.il>
2021-10-27 13:53:16 +0300

    DataSource

--- dotNet5782_0881_3993/DAL/DataSource.cs
+++ dotNet5782_0881_3993/DAL/DataSource.cs
@@ -9,81 +9,88 @@ namespace DalObject
 {
     struct DataSource
     {
-        internal static List<IDAL.DO.Drone> Drones = new List<IDAL.DO.Drone>(10) ;
-        internal static List<IDAL.DO.Station> Stations = new List<IDAL.DO.Station>(5);
-        internal static List<IDAL.DO.Customer> Customers = new List<IDAL.DO.Customer>(100);
-        internal static List<IDAL.DO.Parcel> Parcels = new List< IDAL.DO.Parcel>(1000);
+        public static Random rand = new();
+        
+        #region The entities lists
+        // pay attention - we chose to work with lists, so we don't need to initialize the sizes
+        internal static List<Drone> Drones = new List<Drone>() ;
+        internal static List<Station> Stations = new List<Station>();
+        internal static List<Customer> Customers = new List<Customer>();
+        internal static List<Parcel> Parcels = new List<Parcel>();
+        internal static List<DroneCharge> DroneCharges = new List<DroneCharge>();
+        #endregion The entities lists
+
         internal struct Config
         {
-            internal static int DroneIndex = 0;
-            internal static int StationIndex = 0;
-            internal static int CustomerIndex = 0;
-            internal static int ParcelIndex = 0;
-            public static int RunId;
+            /// <summary> Updating the package amount </summary>
+            public static int RunId = 1000;
         }
 
-        public static Random r = new();
         private static int DroneId;
 
         public static T RandomEnumValue<T>()
         {
             var v = Enum.GetValues(typeof(T));
-            return (T)v.GetValue(r.Next(v.Length));
+            return (T)v.GetValue(rand.Next(v.Length));
         }
 
         public static void Initialize()
         {
+            #region adding Drone details
             for (int i = 0; i < 5; i++)
             {
-                Drones[i] = new Drone()
+                Drones.Add(new Drone()
                 {
-                    Id = Config.DroneIndex++,
+                    Id =rand.Next(1000,10000),
                     Model = "model:" + i.ToString(),
                     MaxWeight = RandomEnumValue<WeightCategories>(),
                     Status = RandomEnumValue<DroneStatuses>(),
-                    Battery = r.NextDouble() * 100
-                };
-
+                    Battery = rand.NextDouble() * 100
+                });
             }
+            #endregion adding Drone details
 
-            Stations[0] = new Station() { Id = Config.StationIndex++, Name = "Haifa Drone Station", Lattitude = 32.794044, Longitude = 34.989571, ChargeSlots = 4 };
-            Stations[1] = new Station() { Id = Config.StationIndex++, Name = "Tel Aviv Drone Station", Lattitude = 32.056312, Longitude = 34.779888, ChargeSlots = 3 };
+            Stations[0] = new Station() { Id = 0 , Name = "Haifa Drone Station", Lattitude = 32.794044, Longitude = 34.989571, ChargeSlots = 4 };
+            Stations[1] = new Station() { Id = 1, Name = "Tel Aviv Drone Station", Lattitude = 32.056312, Longitude = 34.779888, ChargeSlots = 3 };
+
+            #region adding Customer details
             string[] CustomerName = { "Aviad", "Avi", "Evyatar", "Dan", "Gad", "Gal", "John", "Mike", "Eli", "Michael" };
-
-
             for (int i = 0; i < 10; i++)
             {
 
-                Customers[i] = new Customer()
+                Customers.Add(new Customer()
                 {
-                    Id = r.Next(100000000, 1000000000),
-                    Name = CustomerName[r.Next(CustomerName.Length)],
-                    Phone = $"05{ r.Next(2, 9) }{ r.Next(1000000, 10000000) }",
-                    Lattitude = r.NextDouble() * (33.4188709641265 - 29.49970431757609) + 29.49970431757609,
-                    Longitude = r.NextDouble() * (35.89927249423983 - 34.26371323423407) + 34.26371323423407,
-                };
-                Config.CustomerIndex++;
+                    Id = rand.Next(100000000, 1000000000),
+                    Name = CustomerName[rand.Next(CustomerName.Length)],
+                    Phone = $"05{ rand.Next(2, 9) }{ rand.Next(1000000, 10000000) }",
+                    Lattitude = rand.NextDouble() * (33.4188709641265 - 29.49970431757609) + 29.49970431757609,
+                    Longitude = rand.NextDouble() * (35.89927249423983 - 34.26371323423407) + 34.26371323423407,
+                });
             }
+            #endregion adding Customer details
 
 
+
+
+            #region adding Parcel details
             for (int i = 0; i < 10; i++)
             {
-                TimeSpan time = new TimeSpan(0, r.Next(0, 60), r.Next(0, 60));
-                Parcels[i] = new Parcel()
+                TimeSpan time = new TimeSpan(0, rand.Next(0, 60), rand.Next(0, 60));
+                Parcels.Add (new Parcel()
                 {
-                    Id = Config.ParcelIndex++,
-                    SenderId = r.Next(100000000, 1000000000),
-                    TargetId = r.Next(100000000, 1000000000),
+                    Id = rand.Next(0, 100),
+                    SenderId = rand.Next(100000000, 1000000000),
+                    TargetId = rand.Next(100000000, 1000000000),
                     Weight = RandomEnumValue<WeightCategories>(),
                     Priority = RandomEnumValue<Priorities>(),
                     Requested = DateTime.Now,
                     Scheduled = DateTime.Now + time,
                     PickedUp = DateTime.Now + time + time,
                     Delievered = DateTime.Now + time + time + time,
-                    DroneId = Drones[i].Status == DroneStatuses.Free ? r.Next(0, Config.DroneIndex - 1) : -1 //if there is a free drone - random, otherwise an exception will be thrown 
-                };
+                    DroneId = 0,
+                });
             }
-            Config.RunId = Config.ParcelIndex;//Parcel RunId which is bigger than the Parcel amount
+            #endregion adding Parcel details
         }
     };
      
