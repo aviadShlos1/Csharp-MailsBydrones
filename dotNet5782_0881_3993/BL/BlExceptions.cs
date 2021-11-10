6292c1d9eac7cc4c5690da4de0edd1f274bdefed
aviad shlosberg <ashlosbe@g.jct.ac.il>
2021-11-10 11:33:09 +0200

    BL exceptions and IBL

---
+++ dotNet5782_0881_3993/BL/BlExceptions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace IBL.BO
+{
+    [Serializable]
+    public class BaseStationIdException:Exception
+    {
+        public int ID;
+        
+    }
+}
---
+++ dotNet5782_0881_3993/BL/IBL.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IBL
+{
+    interface IBL
+    {
+        #region AddOptions
+
+        public void AddBaseStation();
+        public void AddDrone();
+        public void AddCustomer();
+        public void AddParcel();
+
+        #endregion AddOptions
+
+        #region UpdateOptions
+
+        public void UpdateDroneName();
+        public void UpdateBaseStationData();
+        public void UpdateCustomerData();
+        public void DroneToCharge();
+        public void ReleaseDroneCharge();
+        public void AssignParcelToDrone();
+        public void PickUpParcel();
+        public void SupplyParcel();
+        #endregion UpdateOptions    
+
+        #region DisplayOptions
+        public void BaseStationDisplay();
+        public void DroneDisplay();
+        public void CustomerDisplay();
+        public void ParcelDisplay();
+
+        #endregion DisplayOptions
+
+        #region ListDisplayOptions
+        public void BaseStationsListDisplay();
+        public void DronesListDisplay();
+        public void CustomersListDisplay();
+        public void ParcelsListDisplay();
+        public void ParcelsWithoutDroneListDisplay();
+        public void FreeChargeSlotsListDisplay();
+        #endregion ListDisplayOptions
+    }
+}
