618cd2209fec1892fb38df18de67ba9538c89d66
aviad shlosberg <ashlosbe@g.jct.ac.il>
2021-11-22 13:25:27 +0200

    singleUpdate

--- dotNet5782_0881_3993/BL/SingleDisplayOptions.cs
+++ dotNet5782_0881_3993/BL/SingleDisplayOptions.cs
@@ -3,36 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using IDAL.DO;
+using IBL.BO;
 namespace IBL
 {
     partial class BL
     {
-        public List<BO.DroneInCharge> GetBaseStation(int baseStationId)
+        public BaseStationBL GetBaseStation(int baseStationId)
         {
             IDAL.DO.BaseStation myBaseStation = new();
             try
             {
-                myBaseStation = DalAccess.GetBaseStation(baseStationId);//
+                myBaseStation = DalAccess.GetBaseStation(baseStationId);
             }
-            catch (Exception)
+            catch (IDAL.DO.NotExistException)
             {
-
-                throw;
+               /* throw new BO.NotExistException(baseStationId,"baseStationId",*/ /*);*/
             }
-            int i = 0;
-            BO.Location myLocation = new() { Latitude = myBaseStation.Latitude, Longitude = myBaseStation.Longitude };
-            BO.BaseStationBL myStationBl = new() { Id = myBaseStation.Id, BaseStationName = myBaseStation.Name, Location = myLocation, FreeChargeSlots = myBaseStation.FreeChargeSlots, DronesInChargeList = new() };
+            
+            Location myLocation = new() { Latitude = myBaseStation.Latitude, Longitude = myBaseStation.Longitude };
+            BaseStationBL myStationBl = new() { Id = myBaseStation.Id, BaseStationName = myBaseStation.Name, Location = myLocation, FreeChargeSlots = myBaseStation.FreeChargeSlots, DronesInChargeList = new() };
             
             var dronesInChargePerStation = DalAccess.GetDronesChargeList().TakeWhile(x => x.StationId == baseStationId).ToList();
             foreach (var item in dronesInChargePerStation)
             {
                 var tempDrone = DronesListBL.Find(x => x.DroneId == item.DroneId);
-                myStationBl.DronesInChargeList[i].Id = tempDrone.DroneId;
-                myStationBl.DronesInChargeList[i].BatteryPercent = tempDrone.BatteryPercent;
-                i++;
+                myStationBl.DronesInChargeList.Add( new DroneInCharge { Id = tempDrone.DroneId, BatteryPercent = tempDrone.BatteryPercent });
             }
-            return myStationBl.DronesInChargeList;
+            return myStationBl;
         }
         public List<BO.DroneInShipment> GetDrone(int droneId)
         {
