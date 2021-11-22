5b4a90c1fed568c9b2328c83cacc411eab833bdd
aviad shlosberg <ashlosbe@g.jct.ac.il>
2021-11-22 21:46:24 +0200

    almost

--- dotNet5782_0881_3993/BL/IBL.cs
+++ dotNet5782_0881_3993/BL/IBL.cs
@@ -9,42 +9,42 @@ namespace IBL
 {
     public interface IBL
     {
-        //#region AddOptions
+        #region AddOptions
 
-        //public void AddBaseStation();
-        //public void AddDrone();
-        //public void AddCustomer();
-        //public void AddParcel();
+        public void AddBaseStation(int myId, string myBaseStationName, Location myBaseStationLocation, int myFreeChargeSlots);
+        public void AddDrone(int myDroneId, string myModel, WeightCategoriesBL myDroneWeight, int myBaseStationId);
+        public void AddCustomer();
+        public void AddParcel();
 
-        //#endregion AddOptions
+        #endregion AddOptions
 
-        //#region UpdateOptions
+        #region UpdateOptions
 
-        //public void UpdateDroneName();
-        //public void UpdateBaseStationData();
-        //public void UpdateCustomerData();
-        //public void DroneToCharge();
-        //public void ReleaseDroneCharge();
-        //public void AssignParcelToDrone();
-        //public void PickUpParcel();
-        //public void SupplyParcel();
-        //#endregion UpdateOptions    
+        public void UpdateDroneName();
+        public void UpdateBaseStationData();
+        public void UpdateCustomerData();
+        public void DroneToCharge();
+        public void ReleaseDroneCharge();
+        public void AssignParcelToDrone();
+        public void PickUpParcel();
+        public void SupplyParcel();
+        #endregion UpdateOptions    
 
-        //#region DisplayOptions
-        //public void GetSingleBaseStation();
-        //public void GetSingleDrone();
-        //public void GetSingleCustomer();
-        //public void GetSingleParcel();
+        #region DisplayOptions
+        public void GetSingleBaseStation();
+        public void GetSingleDrone();
+        public void GetSingleCustomer();
+        public void GetSingleParcel();
 
-        //#endregion DisplayOptions
+        #endregion DisplayOptions
 
-        //#region ListDisplayOptions
-        //public void GetBaseStationsList();
-        //public void GetDronesList();
-        //public void GetCustomersList();
-        //public void GetParcelsList();
-        //public void ParcelsWithoutDroneListDisplay();
-        //public void FreeChargeSlotsListDisplay();
-        //#endregion ListDisplayOptions
+        #region ListDisplayOptions
+        public void GetBaseStationsList();
+        public void GetDronesList();
+        public void GetCustomersList();
+        public void GetParcelsList();
+        public void ParcelsWithoutDroneListDisplay();
+        public void FreeChargeSlotsListDisplay();
+        #endregion ListDisplayOptions
     }
 }
