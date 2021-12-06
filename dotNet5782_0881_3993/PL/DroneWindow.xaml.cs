43c5e2578dc56c0300eb12cc5474eda9798c7493
evilevi <evilevi20@gmail.com>
2021-12-06 15:52:10 +0200

    Add

--- dotNet5782_0881_3993/PL/DroneWindow.xaml.cs
+++ dotNet5782_0881_3993/PL/DroneWindow.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+using IBL.BO;
 namespace PL
 {
     /// <summary>
@@ -19,10 +19,43 @@ namespace PL
     /// </summary>
     public partial class DroneWindow : Window
     {
+        private IBL.IBL blAccess;
         public DroneWindow(IBL.IBL blAccessTemp)
         {
             InitializeComponent();
+            blAccess = blAccessTemp;
+            DroneToList newDrone = new({ DroneId=int.Parse(TextBlock.TextProperty)})
+            WeightSelector.ItemsSource = Enum.GetValues(typeof(WeightCategoriesBL));
+            StatusSelector.ItemsSource = Enum.GetValues(typeof(DroneStatusesBL));
+        }
+
+        private void WeightComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            WeightSelector.SelectedItem = Enum.GetValues(typeof(WeightCategoriesBL));
+        }
+        private void StatusComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            StatusSelector.SelectedItem = Enum.GetValues(typeof(DroneStatusesBL));
+        }
+
+        private void Battery_TextChanged(object sender, TextChangedEventArgs e)
+        {
             
+        }
+
+        private void Id_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            blAccess.AddDrone();
         }
     }
 }
