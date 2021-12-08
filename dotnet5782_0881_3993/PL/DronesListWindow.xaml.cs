a7309fad47d99511c6bb488e5b0893f70702d45f
aviad shlosberg <ashlosbe@g.jct.ac.il>
2021-12-08 23:54:50 +0200

    Ex3 Final Commit

--- dotnet5782_0881_3993/PL/DronesListWindow.xaml.cs
+++ dotnet5782_0881_3993/PL/DronesListWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿//Names: Aviad Shlosberg       314960881      
+//       Evyatar Levi Ben Ston 318753993 
+//Targil3
+//brief: In this program we built the presentation layer
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -51,24 +55,51 @@ namespace PL
                 DronesListView.ItemsSource = blAccess.GetDronesBl(x => x.DroneStatus == (DroneStatusesBL)StatusSelector.SelectedItem && x.DroneWeight == (WeightCategoriesBL)WeightSelector.SelectedItem);
             }
         }
+       
+        /// <summary>
+        /// A selection event, the user will choose between the status categories 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ComboBox_StatusSelection(object sender, SelectionChangedEventArgs e)//
         {
             selectionOptions();//
         }
+      
+        /// <summary>
+        /// A selection event, the user will choose between the weight categories 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ComboBox_WeightSelection(object sender, SelectionChangedEventArgs e)
         {
             selectionOptions();
         }
 
+        /// <summary>
+        /// A button click event, the add drone window will be opened
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddDroneButton_Click(object sender, RoutedEventArgs e)
         {
             new DroneWindow(blAccess,this).Show();
         }
+        /// <summary>
+        /// A button click event, the add drone window will be opened
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ClosingWindowButton_Click(object sender, RoutedEventArgs e)
         {          
             new MainWindow().Show();
             this.Close();
         }
+        /// <summary>
+        /// A reset button click event, the selection will be cleared
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void ResetButton_Click(object sender, RoutedEventArgs e)
         {
             StatusSelector.SelectedItem = null;
@@ -76,6 +107,11 @@ namespace PL
             //DroneListView.ItemsSource = droneToLists;
             selectionOptions();
         }
+        /// <summary>
+        /// A double click event. The user will click double click on the wanted drone, in that he will be able to do some action with it
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void DronesListView_DoubleClick(object sender, MouseButtonEventArgs e)
         {
             DroneToList temp = (DroneToList)DronesListView.SelectedItem;
