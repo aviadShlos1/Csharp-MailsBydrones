c128e65cc0e526a88c2b788521162e26eba76a8e
aviad shlosberg <ashlosbe@g.jct.ac.il>
2021-12-27 18:47:59 +0200

    NewMainWindow

--- dotNet5782_0881_3993/PL/MainWindow.xaml.cs
+++ dotNet5782_0881_3993/PL/MainWindow.xaml.cs
@@ -16,8 +16,30 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Collections.ObjectModel;
+using System.Media;
+using System.Windows.Media.Animation;
 using BO;
+//namespace PL
+//{
+//    /// <summary>
+//    /// Interaction logic for MainWindow.xaml
+//    /// </summary>
+//    public partial class MainWindow : Window
+//    {
+//        // access point between the bl to the pl
+//        private BlApi.IBL blAccess = BlApi.BlFactory.GetBl();
+//        public MainWindow()
+//        {
+//            InitializeComponent();
+//            //media.Source = new Uri(Environment.CurrentDirectory + @"\load.gif");
+//        }
+//        private void ClosingBt_Click(object sender, RoutedEventArgs e)
+//        {
+//            this.Close();
+//        }
+//    }
+//}
+
 
 namespace PL
 {
@@ -26,17 +48,80 @@ namespace PL
     /// </summary>
     public partial class MainWindow : Window
     {
-        // access point between the bl to the pl
-        private BlApi.IBL blAccess = BlApi.BlFactory.GetBl();
+
+        #region main window
+        /// <summary> the constractor start the intlize consractor of the data </summary>
         public MainWindow()
         {
             InitializeComponent();
-            //media.Source = new Uri(Environment.CurrentDirectory + @"\load.gif");
         }
-        private void Button_Click(object sender, RoutedEventArgs e)
+
+        // we crate an obejt that give us accses to the ibl intrface  
+        public BlApi.IBL AccessIbl = BlApi.BlFactory.GetBl();
+
+        /// <summary> open the drone list window  </summary>
+        private void Blogin_Click(object sender, RoutedEventArgs e)
         {
-            new ListsDisplayWindow().Show();
-            this.Close();
+
+            switch (Blogin.Content)
+            {
+                case "Enter as Admin":
+                    new ListsDisplayWindow().Show();//(AccessIbl).Show();?????????????????????????????????????????????????????????????????
+                    this.Close(); // we close the login window
+                    break;
+                case "Register":
+
+                    break;
+                case "Enter as Client":
+                    //new CustomersWindow().Show();
+                    //this.Close(); // we close the login window
+                    //break;
+                default:
+
+                    break;
+            }
+            //enter.Unloaded -= enter_Unloaded;
+            //enter.Source = null;
+            //enter.Close();
+        }
+        #endregion
+
+
+
+        //private void enter_Loaded(object sender, RoutedEventArgs e)
+        //{
+        //    AddOn.Opacity = 0;
+        //    DoubleAnimation Animmation = new DoubleAnimation(0, 100, TimeSpan.FromSeconds(10.5));
+        //    PBloding.BeginAnimation(ProgressBar.ValueProperty, Animmation);
+        //}
+
+        //private void PBloding_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        //{
+        //    if (PBloding.Value == 100)
+        //    {
+        //        Blogin.IsEnabled = true;
+        //        DoubleAnimation doubleAnimmation = new DoubleAnimation(0, 1, TimeSpan.FromSeconds(5));
+        //        AddOn.BeginAnimation(Grid.OpacityProperty, doubleAnimmation);
+        //        DoubleAnimation DSF = new DoubleAnimation(1, 0, TimeSpan.FromSeconds(2));
+        //        Disiaper.BeginAnimation(Grid.OpacityProperty, DSF);
+
+        //    }
+        //}
+
+        private void TCadmin_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Blogin.Content = "Enter as Admin";
+        }
+
+        private void TIRegister_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Blogin.Content = "Register";
+
+        }
+
+        private void TabItem_GotFocus(object sender, RoutedEventArgs e)
+        {
+            Blogin.Content = "Enter as Client";
         }
         private void ClosingBt_Click(object sender, RoutedEventArgs e)
         {
@@ -44,3 +129,4 @@ namespace PL
         }
     }
 }
+
