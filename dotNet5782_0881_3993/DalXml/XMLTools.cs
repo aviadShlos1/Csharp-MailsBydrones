89f41ca9a44166adc26dc73039bdb8ec1cc44de3
aviad shlosberg <ashlosbe@g.jct.ac.il>
2022-01-03 14:04:10 +0200

    Save&Load FileFunctions

---
+++ dotNet5782_0881_3993/DalXml/XMLTools.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Dal;
+using System.Xml.Serialization;
+namespace Dal
+{
+    class XMLTools
+    {
+        static string dir = @"xml\";
+        static XMLTools()
+        {
+            if (!Directory.Exists(dir))
+                Directory.CreateDirectory(dir); 
+        }
+        #region SaveLoadWithXElement
+        public static void SaveListToXMLSerializer<T>(List<T> list, string filePath)
+        {
+            try
+            {
+                FileStream file = new FileStream(dir + filePath, FileMode.Create);
+                XmlSerializer x = new XmlSerializer(list.GetType());
+                x.Serialize(file, list);
+                file.Close();
+            }
+            catch (Exception ex)
+            {
+                throw new DalApi.DalConfigException($"fail to create xml file:{filePath}", ex);
+            }
+        }
+        public static List<T> LoadListFromXMLSerializer<T>(string filePath)
+        {
+            try
+            {
+
+                if (File.Exists(dir + filePath))
+                {
+                    List<T> list;
+                    XmlSerializer x = new XmlSerializer(typeof(List<T>));
+                    FileStream file = new FileStream(dir + filePath, FileMode.Open);
+                    list = (List<T>)x.Deserialize(file);
+                    file.Close();
+                    return list;
+                }
+                else
+                    return new List<T>();
+            }
+            catch (Exception ex)
+            {
+
+                throw new DalApi.DalConfigException($"fail to load xml file:{filePath}", ex);
+            }
+        }
+
+        #endregion
+    }
+}
