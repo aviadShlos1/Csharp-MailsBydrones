37404887f61f0c4410217331d4d8788ec45e7d59
evilevi <evilevi20@gmail.com>
2022-01-12 21:58:19 +0200

    simulator

--- dotNet5782_0881_3993/PL/DroneWindow.xaml.cs
+++ dotNet5782_0881_3993/PL/DroneWindow.xaml.cs
@@ -15,7 +15,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Threading;
-using System.Windows.Media.Imaging;
+using System.Windows.Media.Imaging; 
 using System.Windows.Shapes;
 using System.ComponentModel;
 using BO;
