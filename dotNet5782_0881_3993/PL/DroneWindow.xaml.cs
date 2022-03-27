7ff953cd33f56c409d6a7fad73f577d026345d57
evilevi <evilevi20@gmail.com>
2022-03-27 17:48:46 +0300

    design fixes

--- dotNet5782_0881_3993/PL/DroneWindow.xaml.cs
+++ dotNet5782_0881_3993/PL/DroneWindow.xaml.cs
@@ -48,24 +48,24 @@ namespace PL
             baseId = blAccess.GetBaseStationsBl().Select(item => item.Id);
             BaseStationIdSelector.ItemsSource = baseId;
         }
-        /// <summary>
-        /// A selection event, the user will choose between the weight categories 
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void WeightComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            WeightSelector.SelectedItem = Enum.GetValues(typeof(WeightCategoriesBL));
-        }
-        /// <summary>
-        /// A selection event, the user will choose between the status categories 
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void StatusComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            StatusSelector.SelectedItem = Enum.GetValues(typeof(DroneStatusesBL));
-        }
+        ///// <summary>
+        ///// A selection event, the user will choose between the weight categories 
+        ///// </summary>
+        ///// <param name="sender"></param>
+        ///// <param name="e"></param>
+        //private void WeightComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    WeightSelector.SelectedItem = Enum.GetValues(typeof(WeightCategoriesBL));
+        //}
+        ///// <summary>
+        ///// A selection event, the user will choose between the status categories 
+        ///// </summary>
+        ///// <param name="sender"></param>
+        ///// <param name="e"></param>
+        //private void StatusComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        //{
+        //    StatusSelector.SelectedItem = Enum.GetValues(typeof(DroneStatusesBL));
+        //}
         /// <summary>
         /// A selection event, the user will choose between the base stations  
         /// </summary>
